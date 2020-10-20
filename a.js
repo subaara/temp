8d3bd16e91d6d30f0d4efa420264b4b28d26df47
subashtrying <subashtrying@gmail.com>
2020-10-20 17:19:51 +0530

    Appvance Commit [skip ci] - [adding a.js]

---
+++ a.js
@@ -0,0 +1,6 @@
+setVariablesIfNeeded('SERVER/home/kumalti/Appvance/scrip/tickets/AIQ-2750/a.csv','HashDPL',0);
+navigateTo("http://sahitest.com/demo/formTest.htm");
+setValue(textbox("bordercheck"),"aa");
+setValue(password("p1"),"dfdsf");
+setValue(password(1),"ddddddddddddd");
+navigateTo("http://sahitest.com/demo/formTest.htm");
